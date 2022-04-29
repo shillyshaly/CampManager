dbaff356ff0a9d5d04c1369373e6b41236976a7e
adluc12 <70117082+adluc12@users.noreply.github.com>
2022-04-29 16:53:08 -0700

    Update MenuList.java

--- src/MenuList.java
+++ src/MenuList.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class MenuList {
     public static int mainMenu(){
         System.out.println("******************************\n" +
-                "* 1. Camper Menu\n" +
-                "* 2. See campers\n" +
-                "* 3. Delete by name\n" +
-                "* 4. Camper details by name\n" +
+                "* 1. Applications\n" +
+                "* 2. Bunkhouse\n" +
+                "* 3. Tribe\n" +
+                "* 4. Arrival Day\n" +
                 "* 5. Exit\n" +
                 "******************************\n");
         int menuChoice = 0;
@@ -19,9 +19,26 @@ public class MenuList {
     public static int camperMenu(){
         System.out.println("******************************\n" +
                 "**********Camp Menu*********\n" +
-                "* 1. Add a new camper\n" +
-                "* 2. See campers\n" +
-                "* 3. Delete by name\n" +
+                "* 1. Add a new Application\n" +
+                "* 2. See Applicants\n" +
+                "* 3. Delete Applicant\n" +
+                "* 4. Camper details by name\n" +
+                "* 5. Exit\n" +
+                "******************************\n");
+
+        int menuChoice = 0;
+        Scanner scanner = new Scanner(System.in);
+        menuChoice = scanner.nextInt();
+
+        return menuChoice;
+    }
+
+    public static int bunkHouseMenu(){
+        System.out.println("******************************\n" +
+                "**********Bunkhouse Menu*********\n" +
+                "* 1. Add a new Application\n" +
+                "* 2. See Applicants\n" +
+                "* 3. Delete Applicant\n" +
                 "* 4. Camper details by name\n" +
                 "* 5. Exit\n" +
                 "******************************\n");
