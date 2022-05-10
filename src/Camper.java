e2716b48b366f114f98b2beb0bf6bc248e72f583
StephenSnebold <stephen.snebold722@myci.csuci.edu>
2022-05-09 22:06:09 -0700

    Added Some Checks for Applications
    Added checks for applications. Some of it is incomplete. For example when an application is removed, the count in males or females are not removed.

--- src/Camper.java
+++ src/Camper.java
@@ -1,12 +1,25 @@
 //import com.mysql.cj.util.StringUtils;
 
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Camper {
     static PreparedStatement statement = null;
     static Connection connection;
     static ResultSet rs = null;
+
+    static int FJuneCount = 0;
+    static int MJuneCount = 0;
+    static int FAugustCount = 0;
+    static int MAugustCount = 0;
+    static int FJulyCount = 0;
+    static int MJulyCount = 0;
 
 
     static {
@@ -17,7 +30,7 @@ public class Camper {
         }
     }
 
-    public static void newCamper() throws SQLException {
+    public static void newCamper() throws SQLException, ParseException {
 
         String camper = "INSERT INTO camper (camper_fname, camper_lname, camper_age, camper_house, camper_docs)" +
                 " VALUES (?, ?, ?, ?, ?)";
@@ -38,17 +51,161 @@ public class Camper {
         String lname = scanner.next();
         System.out.println("enter age: ");
         int age = scanner.nextInt();
+<<<<<<< Updated upstream
         System.out.println("enter bunk house(a-e): ");
         String house = scanner.next();
         System.out.println("enter completed docs(y/n): ");
         String complete = scanner.next();
+=======
+
+        System.out.println("enter date of birth (ex. yyyy-mm-dd): ");
+        String birthday = scanner.next();
+        LocalDate dob = LocalDate.parse(birthday);
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(dob, today);
+
+        if (period.getYears() < 9 || period.getYears() > 18)
+        {
+            System.out.println("Application Rejected. Either too young or too old to join camp");
+            return;
+        }
+
+        System.out.println("enter gender (ex. male/female): ");
+        String gender = scanner.next();
+>>>>>>> Stashed changes
+
+        System.out.println("enter session month by typing the month the session takes place. (August, June, or July");
+        String session = scanner.next();
+
+        if (session.toLowerCase().equals("august"))
+        {
+            int totalCamp = MAugustCount + FAugustCount;
+            if (totalCamp < 72)
+            {
+                System.out.println("Too many Campers in this session. Try filing out an application for another session.");
+                return;
+            }
+
+            if(gender.toLowerCase().equals("male"))
+            {
+                if(MAugustCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    MAugustCount++;
+                }
+
+            }
+            if (gender.toLowerCase().equals("female"))
+            {
+                if(FAugustCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    FAugustCount++;
+                }
+            }
+
+        }
+
+        if (session.toLowerCase().equals("july"))
+        {
+            int totalCamp = MJulyCount + FJulyCount;
+            if (totalCamp < 72)
+            {
+                System.out.println("Too many Campers in this session. Try filing out an application for another session.");
+                return;
+            }
+
+            if(gender.toLowerCase().equals("male"))
+            {
+                if(MJulyCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    MJulyCount++;
+                }
+
+            }
+            if (gender.toLowerCase().equals("female"))
+            {
+                if(FAugustCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    FJulyCount++;
+                }
+            }
+
+        }
+
+        if (session.toLowerCase().equals("june"))
+        {
+            int totalCamp = MJuneCount + FJuneCount;
+            if (totalCamp == 72)
+            {
+                System.out.println("Too many Campers in this session. Try filing out an application for another session.");
+                return;
+            }
+
+            if(gender.toLowerCase().equals("male"))
+            {
+                if(MJuneCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    MJuneCount++;
+                }
+
+            }
+            if (gender.toLowerCase().equals("female"))
+            {
+                if(FJuneCount < 32)
+                {
+                    return;
+                }
+                else
+                {
+                    FJuneCount++;
+                }
+            }
+
+
+        }
+
+
+
+
+        System.out.println("enter if user paid check(y/n): ");
+        String complete = scanner.next();
+
+        if (complete.toLowerCase().equals("n"))
+        {
+            System.out.println("Application rejected, pay next time");
+            return;
+        }
+
 
         //sql insert statement for camper table (test)
         statement = connection.prepareStatement(camper);
         statement.setString(1, fname);
         statement.setString(2, lname);
         statement.setInt(3, age);
+<<<<<<< Updated upstream
         statement.setString(4, house);
+=======
+        statement.setString(4, birthday);
+>>>>>>> Stashed changes
         statement.setString(5, complete);
 
 //        System.out.println(statement);
@@ -58,7 +215,121 @@ public class Camper {
         statement.executeBatch();
         connection.commit();
     }
+    /*public static void applicationDate() throws ParseException, SQLException {
 
+<<<<<<< Updated upstream
+=======
+        //enter application date
+        System.out.print("Enter application date (int MM-dd format): ");
+        Scanner inputDate = new Scanner(System.in);
+        String appDate = inputDate.nextLine();
+
+        //select month option
+        System.out.print(" 1. June\n" +
+                " 2. July\n" +
+                " 3. August\n" +
+                "Select month: ");
+        Scanner date = new Scanner(System.in);
+        int campDate = date.nextInt();
+        getDateDiff(appDate, campDate);
+
+        //enter if check cleared
+        System.out.print("Did the check clear?: ");
+        Scanner clearedCheck = new Scanner(System.in);
+        char clrChk = clearedCheck.next().toLowerCase().charAt(0);
+        checkPayment(clrChk);
+
+        //check in
+        checkIn();
+    }
+
+    public static void checkPayment(char ans) throws ParseException, SQLException {
+        //did the payment go through
+        if(ans == 'n'){
+            sendReject();
+        }else if(ans == 'y'){
+            sendAccept();
+        }else{
+            System.out.println("something went wrong...");
+        }
+    }
+
+    public static void checkIn(){
+        System.out.print("Camper's forms verified? (y/n): ");
+        Scanner scanner = new Scanner(System.in);
+        String forms = scanner.next();
+
+        if (forms.toLowerCase().equals("y")){
+            System.out.println("Camper is good to go...\n");
+        }else if (forms.toLowerCase().equals("n")){
+            System.out.println("Parent must be notified of incomplete items.");
+        }else{
+            System.out.println("Something went wrong.");
+        }
+
+    }
+
+    public static void getDateDiff(String date, int campDate) throws ParseException, SQLException {
+//        Date today = Calendar.getInstance().getTime();
+//        System.out.println("todays date: " + today);
+
+        java.util.Date month = null;
+
+        switch (campDate){
+            case 1:
+                month = new SimpleDateFormat("MM-dd").parse("06-15");
+                break;
+            case 2:
+                month = new SimpleDateFormat("MM-dd").parse("07-15");
+                break;
+            case 3:
+                month = new SimpleDateFormat("MM-dd").parse("08-15");
+                break;
+            default:
+                System.out.println("incorrect option...");
+                System.exit(0);
+                break;
+        }
+
+        Date applicationDate = new SimpleDateFormat("MM-dd").parse(date);
+
+        long dateDiff = month.getTime() - applicationDate.getTime();
+        long days = dateDiff/(1000*60*60*24);
+
+        if(!(days >= 60 && days <= 240)){
+            sendReject();
+        }
+    }
+
+
+    public static int checkAge(LocalDate bday) throws ParseException, SQLException {
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(bday, today);
+
+        if (period.getYears() < 18 && period.getYears() > 9) {
+            applicationDate();
+        }
+        else{
+            sendReject();
+        }
+        return period.getYears();
+    }
+
+    public static void sendAccept() {
+        //send acceptance package
+        System.out.println("Congrats on acceptance. Arrival packet sent.");
+    }
+
+    //send rejection docs
+    public static void sendReject() throws ParseException, SQLException {
+        //send rejection
+        System.out.println("Does not qualify...\n" + "Rejection letter sent.");
+        MenuList.camperMenu();
+//        System.exit(0);
+    }
+*/
+
+>>>>>>> Stashed changes
     public static void delCamper() throws SQLException {
         System.out.println("Enter first name of camper to be deleted: ");
         Scanner scanner = new Scanner(System.in);
@@ -100,6 +371,8 @@ public class Camper {
         }
         statement.close();
         System.out.println("\n\n");
+        System.out.println("There are" + MAugustCount + "males and " + FAugustCount + " females in August.\n There are "+ MJuneCount + " males and " + FJuneCount + " females in June.\n There are " + MJulyCount + " males and " + FJulyCount + "females in July.");
+
     }
 
     public static ResultSet getCamperDeats(String fname, String lname) throws SQLException {
--- src/MenuList.java
+++ src/MenuList.java
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+import java.sql.SQLException;
+import java.text.ParseException;
+>>>>>>> Stashed changes
 import java.util.Scanner;
 
 public class MenuList {
@@ -8,7 +13,11 @@ public class MenuList {
                 "* 3. Delete by name\n" +
                 "* 4. Camper details by name\n" +
                 "* 5. Exit\n" +
+<<<<<<< Updated upstream
                 "******************************\n");
+=======
+                "******************************\n\n\n");
+>>>>>>> Stashed changes
         int menuChoice = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +25,7 @@ public class MenuList {
         return menuChoice;
     }
 
+<<<<<<< Updated upstream
     public static int camperMenu(){
         System.out.println("******************************\n" +
                 "**********Camp Menu*********\n" +
@@ -25,6 +35,50 @@ public class MenuList {
                 "* 4. Camper details by name\n" +
                 "* 5. Exit\n" +
                 "******************************\n");
+=======
+    public static int camperMenu() throws SQLException, ParseException {
+        System.out.println("******************************\n" + "*      Camper Menu           *\n" +
+                "+****************************+\n" +
+                "* 1. Add a new camper\n" +
+                "* 2. See campers\n" +
+                "* 3. Delete by name\n" +
+                "* 4. Exit\n" +
+                "******************************\n\n\n");
+        int menuChoice = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        menuChoice = scanner.nextInt();
+
+        switch (menuChoice){
+            case 1:
+                Camper.newCamper();
+                break;
+            case 2:
+                Camper.getCampers();
+                break;
+            case 3:
+                Camper.delCamper();
+                break;
+            case 4:
+                System.out.println("Leaving Camper Menu...");
+                break;
+        }
+
+        return menuChoice;
+    }
+
+    public static int tribesMenu() throws SQLException {
+        System.out.println("******************************\n" + "*      Tribes Menu           *\n" +
+                "+****************************+\n" +
+                "* 1. Add camper to tribe\n" +
+                "* 2. Move camper to new tribe\n" +
+                "* 3. Return tribe count\n" +
+                "* 4. Display tribe members\n" +
+
+                "* 5. Exit\n" +
+                "******************************\n\n\n");
+        int menuChoice = 0;
+>>>>>>> Stashed changes
 
         int menuChoice = 0;
         Scanner scanner = new Scanner(System.in);
